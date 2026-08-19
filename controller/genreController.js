32648454e2452200b529811c18611ea3b8ebbc1f
upi1682 <upi538287@gmail.com>
2026-08-19 13:43:15 +0700

    add getAllGenre function to fetch all genres from the database

--- controller/genreController.js
+++ controller/genreController.js
@@ -1,3 +1,14 @@
+const db = require("../models");
+
+async function getAllGenre(req, res) {
+  try {
+    const genre = await db.Genre.findAll();
+    res.status(200).json(genre);
+  } catch (err) {
+    console.error("error fetching genre: ", err.message);
+    res.status(500).json({ error: "failed to fetch genre" });
+  }
+}
 
 
 
